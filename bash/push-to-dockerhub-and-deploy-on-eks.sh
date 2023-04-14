63a165fa6bdef07da51ba6e02192ce7b8475a9d0
ANSHU KUMAR <anshukushwaha824@gmail.com>
2023-04-14 23:59:50 +0530

    added bash file

---
+++ bash/push-to-dockerhub-and-deploy-on-eks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# Define the Docker image names and tags
+image1="linux-tweet-app"
+tag1="latest"
+
+# delete existing images 
+docker rmi anshuhtwt/${image1}
+
+# Build and tag the Docker images
+docker build -t anshuhtwt/${image1}:${tag1} 
+
+# Push the Docker images to Docker Hub
+docker push anshuhtwt/${image1}:${tag1}
+
+# apply yaml file
+kubectl apply -R -f /var/lib/jenkins/workspace/linux-tweet-app/bash/push-to-dockerhub-and-deploy-on-eks.sh
+kubectl apply -R -f /var/lib/jenkins/workspace/linux-tweet-app/k8s-manifest/
